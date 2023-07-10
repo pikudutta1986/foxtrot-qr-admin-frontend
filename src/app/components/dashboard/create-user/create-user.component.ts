e9a64dbac2dca66e27c5a998a15d8a7b3f4fd12b
pikudutta1986 <dutta.ani007@gmail.com>
2023-07-10 18:09:39 +0530

    extra field added to user create api

--- src/app/components/dashboard/create-user/create-user.component.ts
+++ src/app/components/dashboard/create-user/create-user.component.ts
@@ -14,6 +14,27 @@ export class CreateUserComponent {
   plans:any = [];
   selectedPlan:any;
 
+  platforms:any = [
+    {
+      id:'web',
+      text: 'Web'
+    },
+    {
+      id:'android',
+      text: 'Android'
+    },
+    {
+      id:'ios',
+      text: 'iOS'
+    },
+  ];
+  prices:any = [];
+
+  enableDetails:boolean = false;
+  enablePriceDetails:boolean = false;
+  platformError:boolean = false;
+  priceError:boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
     public helperService: HelperService) { }
@@ -24,6 +45,8 @@ export class CreateUserComponent {
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       plan_id: ['',[Validators.required]],
+      platform: [''],
+      price: [''],
     });
 
     this.getPlans();
@@ -36,12 +59,95 @@ export class CreateUserComponent {
 
   // add new user
   addUser() {
+    this.platformError = false;
+    this.priceError = false;
+
+    let formValue = this.registerForm.value;
+    // console.log(this.registerForm.value)
+    if(this.enableDetails) {
+      if(formValue.platform == '') {
+        this.platformError = true;
+      }
+  
+      if(formValue.price == '') {
+        this.priceError = true;        
+      }
+
+      if(formValue.platform && formValue.price) {
+        this.finalSubmit();
+      }
+
+    } else {
+      this.finalSubmit();
+    }
+  }
+
+  getPlans() {
+    if(this.helperService.allPlans && this.helperService.allPlans.length > 0) {
+      let plans = this.helperService.allPlans;
+      this.plans = plans;
+      this.selectedPlan = this.plans[0].id;
+      this.registerForm.controls.plan_id.setValue(this.selectedPlan);
+    } else {
+      setTimeout(() => {
+        this.getPlans();        
+      }, 1000);
+    }
+  }
+
+  onChangePlan(e:any) {
+    this.enablePriceDetails = false;
+    let res = this.plans.find((x:any) => {
+      if(x.id == e) {
+        return x;
+      }
+    });
+    if(res) {
+      if(!res.is_custom && !res.is_free) {
+        this.enableDetails = true;
+        this.registerForm.controls.platform.setValue('');
+        this.registerForm.controls.price.setValue('');
+        console.log('done')
+      } else {
+        this.enableDetails = false;
+        this.registerForm.controls.platform.setValue('');
+        this.registerForm.controls.price.setValue('');
+      }
+    }
+  }
+
+  onChangePlatform(e:any) {
+    let pricing = this.helperService.allPricings;
+    let plan_id = this.registerForm.controls['plan_id'].value;
+    let result = pricing.filter((x:any) => {
+      if(x.plan_id == plan_id && x.platform == e) {
+        return x;
+      }
+    });
+
+    if(result && result.length > 0) {
+      this.prices = result;
+      this.enablePriceDetails = true;
+      this.platformError = false;
+      this.registerForm.controls.price.setValue('');
+    }
+    console.log(result)
+  }
+
+  onChangePrice(e:any) {
+    this.priceError = false;
+    console.log(e);
+  }
+
+  finalSubmit() {
     this.helperService.showloader();
     this.helperService.post('auth/admin/users', this.registerForm.value).subscribe((res: any) => {
       if (res.status) {
         this.helperService.hideloader();
         this.registerForm.reset();
         this.registerForm.controls.plan_id.setValue(this.selectedPlan);
+        this.enableDetails = false;
+        this.enablePriceDetails = false;
         this.helperService.snackPositionTopCenter(res.message);   
         this.helperService.allUsers = [];
         this.helperService.getAllUsers();     
@@ -57,27 +163,6 @@ export class CreateUserComponent {
         console.error('error caught in component');       
       }
     );
-
-  }
-
-  getPlans() {
-    if(this.helperService.allPlans && this.helperService.allPlans.length > 0) {
-      let plans = this.helperService.allPlans;
-      plans.map((x:any) => {
-        if(x.id == 1) {
-          x.disabled = false;
-        } else {
-          x.disabled = true;
-        }
-      });
-      this.plans = plans;
-      this.selectedPlan = this.plans[0].id;
-      this.registerForm.controls.plan_id.setValue(this.selectedPlan);
-    } else {
-      setTimeout(() => {
-        this.getPlans();        
-      }, 1000);
-    }
   }
 
 }
